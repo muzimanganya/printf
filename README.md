8a575cf62f94e17994430724c9fb210b58b9f42a
LAP Ltd <info@lapafrica.com>
2022-07-12 21:28:21 +0200

    Printf

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# printf
+# 0x11. C - printf
+## Description
+
+This team project is part of the first year curriculum of Alx School.
+
+_printf replicates the C standard library printf() function.
+***What you should learn from this project:***
+- How to use git in a team setting
+- Applying variadic functions to a big project
+- The complexities of printf
+- Managing a lot of files and finding a good workflow
+
+## Prototype
+* int _printf(const char *format, ...);*
+## Usage
+- Prints a string to the standard output, according to a given format
+- All files were created and compiled on Ubuntu 14.04.4 LTS using GCC 4.8.4 with the command gcc -Wall -Werror -Wextra -pedantic *.c
+- Returns the number of characters in the output string on success, -1 otherwise
+- Call it this way: _printf("format string", arguments...) where format string can contain conversion specifiers and flags, along with regular characters
+## Examples
+- _printf("Hello, Alx School\n") prints "Hello, Alx School", followed by a new line
+- _printf("%s", "Hello") prints "Hello"
+- _printf("This is a number: %d", 98) prints "This is a number: 98"
+## Authors
+> - Desire Habimana - habdes1@gmail.com
+> - Daka Sode - dakadegu11@gmail.com
--- _printf.c
+++ _printf.c
@@ -1,51 +1,82 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
- *
- * Return: number of chars printed.
+ * find_correct_func - finding the format for _printf
+ * @format: format
+ * Return: NULL
+ */
+
+int (*find_correct_func(const char *format))(va_list)
+{
+unsigned int i = 0;
+code_f find_f[] = {
+{"c", print_char},
+{"s", print_string},
+{"i", print_int},
+{"d", print_dec},
+{"r", print_rev},
+{"b", print_bin},
+{"u", print_unsigned},
+{"o", print_octal},
+{"x", print_hex},
+{"X", print_HEX},
+{"R", print_rot13},
+{"S", print_S},
+{"p", print_p},
+{NULL, NULL}
+};
+
+while (find_f[i].sc)
+{
+if (find_f[i].sc[0] == (*format))
+return (find_f[i].f);
+i++;
+}
+return (NULL);
+}
+
+/**
+ * _printf - produces an output based on format
+ * @format: format
+ * Return: size
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
-
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
-		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
-	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+va_list list;
+int (*f)(va_list);
+unsigned int i = 0, len = 0;
+if (format == NULL)
+return (-1);
+va_start(list, format);
+while (format[i])
+{
+while (format[i] != '%' && format[i])
+{
+_putchar(format[i]);
+len++;
+i++;
+}
+if (format[i] == '\0')
+return (len);
+f = find_correct_func(&format[i + 1]);
+if (f != NULL)
+{
+len += f(list);
+i += 2;
+continue;
+}
+if (!format[i + 1])
+return (-1);
+_putchar(format[i]);
+len++;
+if (format[i + 1] == '%')
+i += 2;
+else
+i++;
+}
+va_end(list);
+return (len);
 }
